4238b3a0115dd15bd40e4bdbf0566083d6d29997
ekopurnama25 <ekobeberapa123@gmail.com>
2023-04-09 15:39:47 +0700

    first commit

--- js/latihan1.js
+++ js/latihan1.js
@@ -1,1 +1,43 @@
-alert("hello World");
+//======================================
+// Control Flow
+//======================================
+var angka = prompt("Masukkan angka");
+if (angka % 2 === 0) {
+  alert(angka + " Genap");
+} else {
+  alert(angka + " Ganjil");
+}
+
+// Perulangan
+// alert("Mulai");
+// for (var i = 0; i < 5; i++) {
+//   alert("Hello World");
+// }
+// alert("Selesai");
+//======================================
+// POP UP BOX
+//======================================
+// alert("Selamat Datang");
+// var lagi = true;
+
+// while adalah pengulangan yang dilakukan berulang ulang
+// while (lagi === true) {
+//   var nama = prompt("Masukan Nama");
+//   alert("hallo" + nama);
+
+//   lagi = confirm("Coba Lagi ?");
+// }
+
+// alert("Terimakasih");
+
+// var tes = confirm("kamu Yakin");
+// if (tes === true) {
+//   alert("Users Menekan Tombol OK");
+// } else {
+//   alert("Users Menekan Tombol Cancel");
+// }
+// console.log(tes);
+
+// const nama = prompt("masukan nama");
+// alert(nama);
+// console.log(nama);
